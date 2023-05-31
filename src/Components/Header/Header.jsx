6e217e47b75b8da20222a4a8fa04c6d977c61b2c
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-05-31 16:55:06 +0330

    fix cart bug

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -53,8 +53,8 @@ export default function Header() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                 </svg>
               </div>
-              {/* opacity-0 invisible */}
-              <div className='absolute group-hover:opacity-100 group-hover:visible top-full left-0 p-5  w-[400px] text-zinc-700 dark:text-white text-base bg-white dark:bg-zinc-700 rounded-2xl border-t border-t-orange-300 space-y-4 tracking-normal shadow-normal transition-all'>
+              {/*  */}
+              <div className='absolute opacity-0 invisible group-hover:opacity-100 group-hover:visible top-full left-0 p-5  w-[400px] text-zinc-700 dark:text-white text-base bg-white dark:bg-zinc-700 rounded-2xl border-t border-t-orange-300 space-y-4 tracking-normal shadow-normal transition-all'>
                 <div className='flex items-center justify-between font-DanaMedium text-xs tracking-tighter'>
                   <span className='text-gray-300'>1 مورد</span>
                   <a href="#" className='flex items-center text-orange-300'>
