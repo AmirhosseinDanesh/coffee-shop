be4c28729d1152418380b65f9b30c1134d95754a
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-06-04 12:10:24 +0330

    add loading for articles

--- src/Pages/Client/Articles/ArticlesClient.jsx
+++ src/Pages/Client/Articles/ArticlesClient.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import Slider from '../../../Components/Slider/Slider'
 import Article from '../../../Components/Article/Article'
 import { DataUrlV1 } from "../../../Data/Data"
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 export default function ArticlesClient() {
   const [articles, setArticles] = useState([])
   useEffect(() => {
@@ -14,16 +17,76 @@ export default function ArticlesClient() {
       <Slider sliders={[{ id: 1, src: "/images/sliders/3.jpg", alt: "قهوه فوری" }]} />
 
       <div className='mt-20 p-10'>
-        <div className='font-DanaBold text-3xl text-center border-t-2 pt-5'>
+        <div className='font-DanaBold text-3xl text-center border-t-2 pt-5 dark:text-gray-300'>
           آخرین مقالات
         </div>
         <div className='flex flex-row p-4'>
           {
-            articles.map((art) => (
-              <Article {...art} />
-            ))
-          }
+            (articles.lenght ? (
+              articles.map((art) => (
+                <Article {...art} />
+              ))
+            ) : (
+              <div className='flex w-full flex-row justify-around p-4'>
+                <div className="max-w-sm basis-1/4 border rounded-lg shadow">
+                  <Skeleton animation="wave" height={200} />
+                  <div className="p-5">
+                    <a href="#">
+                      <h5 className="mb-2 ">
+                        <Skeleton animation="wave" />
+                      </h5>
+                    </a>
+                    <p className="mb-3">
+                      <Skeleton animation="wave" count={2} />
+                    </p>
+                    <span href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-400 cursor-not-allowed rounded-lg   dark:bg-blue-400  ">
+                      ادامه مطلب
+                      <svg aria-hidden="true" className="w-4 h-4 mr-4 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </span>
+                  </div>
+                </div>
+                <div className="max-w-sm basis-1/4 border rounded-lg shadow">
+                  <Skeleton animation="wave" height={200} />
+                  <div className="p-5">
+                    <a href="#">
+                      <h5 className="mb-2 ">
+                        <Skeleton animation="wave" />
+                      </h5>
+                    </a>
+                    <p className="mb-3">
+                      <Skeleton animation="wave" count={2} />
+                    </p>
+                    <span href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-400 cursor-not-allowed rounded-lg   dark:bg-blue-400  ">
+                      ادامه مطلب
+                      <svg aria-hidden="true" className="w-4 h-4 mr-4 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </span>
+                  </div>
+                </div>
+                <div className="max-w-sm basis-1/4 border rounded-lg shadow">
+                  <Skeleton animation="wave" height={200} />
+                  <div className="p-5">
+                    <a href="#">
+                      <h5 className="mb-2 ">
+                        <Skeleton animation="wave" />
+                      </h5>
+                    </a>
+                    <p className="mb-3">
+                      <Skeleton animation="wave" count={2} />
+                    </p>
+                    <span href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-400 cursor-not-allowed rounded-lg   dark:bg-blue-400  ">
+                      ادامه مطلب
+                      <svg aria-hidden="true" className="w-4 h-4 mr-4 rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            )
+            )
 
+            // articles.map((art) => (
+            //   <Article {...art} />
+            // ))
+          }
         </div>
       </div>
     </>
