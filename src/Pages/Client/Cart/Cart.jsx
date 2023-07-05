79f0e1c02ea75f27c8a7ecd02b962fa16a96cfdc
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-07-05 19:31:17 +0330

    Cart page Done

--- src/Pages/Client/Cart/Cart.jsx
+++ src/Pages/Client/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import ProductsContext from '../../../Context/ProductsContext'
 import { Input } from 'postcss'
 export default function Cart() {
     const contextData = useContext(ProductsContext)
-    
+
     const calculateTotalPrice = () => {
         return contextData.userCart.reduce((total, product) => {
             return total + (product.price * product.count);
@@ -96,43 +96,70 @@ export default function Cart() {
         <>
             <Headers />
             <div className='container'>
-                <div className='mt-10 md:mt-48 dark:text-white font-MorabbaBold text-lg md:text-5xl '>
+                <div className='mt-10 md:mt-48 dark:text-white font-MorabbaBold text-2xl md:text-5xl text-center md:text-right'>
                     سبد خرید
                 </div>
             </div>
             <div className="h-screen pt-20">
                 <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0 gap-x-10">
-                    <div className="rounded-lg md:w-2/3">
+                    <div className="rounded-lg md:w-[55%]">
                         {
-                            contextData.userCart.map(product => (
-                                <div className="justify-between items-center mb-6 rounded-lg bg-white dark:bg-zinc-700 p-6 shadow-md sm:flex sm:justify-start">
-                                    <img src={`${DataUrl}/courses/covers/${product.cover}`} alt="product-image" className="w-full rounded-lg sm:w-40" />
-                                    <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                                        <div className="mt-5 sm:mt-0">
-                                            <h2 className="text-lg font-bold text-gray-900 dark:text-white ">{product.name}</h2>
-                                            <p className="mt-1 text-base text-gray-700 dark:text-white "><span className='text-xs'>فی</span> {product.price.toLocaleString()} <span className='mr-1 text-xs'>تومان</span></p>
-                                        </div>
-                                        <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                                            <div className="flex items-center border-gray-100">
-                                                <span className="cursor-pointer rounded-r bg-white py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={()=>addToCart(product)}> + </span>
-                                                <span className="w-8 h-8 flex justify-center items-center  bg-white text-center text-xs outline-none">{product.count}</span>
-                                                <span className="cursor-pointer rounded-l bg-white py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={()=> removeProducts(product)}> - </span>
+                            contextData.userCart.length ? (
+                                contextData.userCart.map(product => (
+                                    <div className="justify-between items-center mb-6 rounded-lg bg-white dark:bg-gray-700 p-6 shadow-md sm:flex sm:justify-start">
+                                        <img src={`${DataUrl}/courses/covers/${product.cover}`} alt="product-image" className="w-full rounded-lg sm:w-40" />
+                                        <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                                            <div className="mt-5 sm:mt-0">
+                                                <h2 className="text-lg font-bold text-gray-900 dark:text-white ">{product.name}</h2>
+                                                <p className="mt-1 text-base text-gray-700 dark:text-white "><span className='text-xs'>فی</span> {product.price.toLocaleString()} <span className='mr-1 text-xs'>تومان</span></p>
                                             </div>
-                                            <div className="flex items-center justify-between w-[30%] md:w-full dark:text-white">
-                                                <p className="text-sm">{(product.price * product.count).toLocaleString()}</p>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-4 md:h-5 w-4 md:w-5 cursor-pointer duration-150 hover:text-red-500" onClick={()=>removeProductFromCart(product)}>
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                                </svg>
+                                            <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                                                <div className="flex items-center border-gray-100">
+                                                    <span className="cursor-pointer rounded-r bg-white py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => addToCart(product)}> + </span>
+                                                    <span className="w-8 h-8 flex justify-center items-center  bg-white text-center text-xs outline-none">{product.count}</span>
+                                                    <span className="cursor-pointer rounded-l bg-white py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => removeProducts(product)}> - </span>
+                                                </div>
+                                                <div className="flex items-center justify-between w-[30%] md:w-full dark:text-white">
+                                                    <p className="text-sm">{(product.price * product.count).toLocaleString()}</p>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-4 md:h-5 w-4 md:w-5 cursor-pointer duration-150 hover:text-red-500" onClick={() => removeProductFromCart(product)}>
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                                    </svg>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
+                                ))
+                            ) : (
+                                <div className="rounded-lg bg-white dark:bg-gray-700 p-6 shadow-md dark:text-white text-center ">
+                                    محصولی در سبد خرید نیست.
                                 </div>
-                            ))
+                            )
                         }
                     </div>
-                    <div className="mt-6 h-full rounded-lg border dark:border-none bg-white dark:bg-zinc-700 p-6 shadow-md md:mt-0 md:w-1/3">
-                        {/* <input type="text" className='input' /> */}
-                        <div className="flex justify-between">
+                    <div className="mt-6 h-full rounded-lg border dark:border-none bg-white dark:bg-gray-700 p-6 shadow-md md:mt-0 md:w-[45%]">
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-3 '>
+                            <div className='md:col-start-1 md:col-end-3'>
+                                <label className='input-label'>نام و نام خانوادگی</label>
+                                <input type="text" className='input' />
+                            </div>
+                            <div>
+                                <label className='input-label'>استان</label>
+                                <input type="text" className='input' />
+                            </div>
+                            <div>
+                                <label className='input-label'>شهر</label>
+                                <input type="text" className='input' />
+                            </div>
+                            <div className='md:col-start-1 md:col-end-3'>
+                                <label className='input-label'>آدرس</label>
+                                <input type="text" className='input' />
+                            </div>
+                            <div className='md:col-start-1 md:col-end-3'>
+                                <label className='input-label'>کدپستی</label>
+                                <input type="text" className='input' />
+                            </div>
+                        </div>
+                        <div className="flex justify-between mt-3 md:mt-10">
                             <p className="text-lg font-bold dark:text-white">مجموع</p>
                             <div className="">
                                 <p className="mb-1 text-lg font-bold dark:text-white">{calculateTotalPrice().toLocaleString()}<span className='text-xs mr-1'>تومان</span></p>
