35409d8199008b3d1bef0b30602b03daacb6dbab
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-07-08 14:03:26 +0330

    update cart with discount

--- src/Pages/Client/Cart/Cart.jsx
+++ src/Pages/Client/Cart/Cart.jsx
@@ -8,9 +8,9 @@ export default function Cart() {
 
     const calculateTotalPrice = () => {
         return contextData.userCart.reduce((total, product) => {
-            return total + (product.price * product.count);
+          return total + (((product.price - ((product.discount * product.price) / 100)) * product.count));
         }, 0);
-    }
+      }
 
     const updateLocalStorage = (cart) => {
         localStorage.setItem('userCart', JSON.stringify(cart));
@@ -18,39 +18,40 @@ export default function Cart() {
 
     const addToCart = (pro) => {
         let newUserProductCart = {
-            id: pro._id,
-            name: pro.name,
-            price: pro.price,
-            count: 1,
-            cover: pro.cover,
+          id: pro._id,
+          name: pro.name,
+          price: pro.price,
+          count: 1,
+          cover: pro.cover,
+          discount: pro.discount
+    
         }
-
+    
         let isProductInCart = contextData.userCart.some(product => (
-            product.name === pro.name
+          product.name === pro.name
         ))
         if (!isProductInCart) {
-            let updatedCart = [...contextData.userCart, newUserProductCart];
-            updateLocalStorage(updatedCart);
-            contextData.setUserCart(updatedCart);
-            console.log("first")
-
+          let updatedCart = [...contextData.userCart, newUserProductCart];
+          updateLocalStorage(updatedCart);
+          contextData.setUserCart(updatedCart);
+    
         } else {
-            let updatedCart = contextData.userCart.map(product => {
-                if (product.name === pro.name) {
-                    return {
-                        ...product,
-                        count: product.count + 1
-
-                    };
-                }
-
-                return product;
-            });
-
-            contextData.setUserCart(updatedCart);
-            updateLocalStorage(updatedCart);
+          let updatedCart = contextData.userCart.map(product => {
+            if (product.name === pro.name) {
+              return {
+                ...product,
+                count: product.count + 1
+    
+              };
+            }
+    
+            return product;
+          });
+    
+          contextData.setUserCart(updatedCart);
+          updateLocalStorage(updatedCart);
         }
-    }
+      }
 
     const removeProducts = (pro) => {
         const updatedCart = contextData.userCart.map(product => {
@@ -111,7 +112,7 @@ export default function Cart() {
                                         <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
                                             <div className="mt-5 sm:mt-0">
                                                 <h2 className="text-lg font-bold text-gray-900 dark:text-white ">{product.name}</h2>
-                                                <p className="mt-1 text-base text-gray-700 dark:text-white "><span className='text-xs'>فی</span> {product.price.toLocaleString()} <span className='mr-1 text-xs'>تومان</span></p>
+                                                <p className="mt-1 text-base text-gray-700 dark:text-white "><span className='text-xs'>فی</span> {(product.price - ((product.discount * product.price) / 100)).toLocaleString()} <span className='mr-1 text-xs'>تومان</span></p>
                                             </div>
                                             <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                                                 <div className="flex items-center border-gray-100">
@@ -120,7 +121,7 @@ export default function Cart() {
                                                     <span className="cursor-pointer rounded-l bg-white py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50" onClick={() => removeProducts(product)}> - </span>
                                                 </div>
                                                 <div className="flex items-center justify-between w-[30%] md:w-full dark:text-white">
-                                                    <p className="text-sm">{(product.price * product.count).toLocaleString()}</p>
+                                                    <p className="text-sm">{((product.price - ((product.discount * product.price) / 100)) * product.count).toLocaleString()}</p>
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-4 md:h-5 w-4 md:w-5 cursor-pointer duration-150 hover:text-red-500" onClick={() => removeProductFromCart(product)}>
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                                                     </svg>
